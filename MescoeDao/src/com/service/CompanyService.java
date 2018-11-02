2f44ffb32c821d4b002c68bf4eb3daa9f9327ebf
Your Name <you@example.com>
2018-11-02 12:03:34 +0530

    Mescoe's CompanyService

--- MescoeDao/src/com/service/CompanyService.java
+++ MescoeDao/src/com/service/CompanyService.java
@@ -4,10 +4,47 @@ import java.util.List;
 
 import com.model.Company;
 
+/**
+ * Interface class that has the following methods
+ * @author Administrator
+ *
+ */
 public interface CompanyService {
-public void addCompanyService(Company company);
-public List<Company>getAllCompanyService();
-public Company getCompanyService(int cid);
-public List<Company> getCompanyByYear(int year);
-public boolean checkCompanyService(int cid);
+	/**
+	 * Function declaration for addCompanyService to add details 
+	 * of a company using the company's object.
+	 * @param company
+	 */
+	public void addCompanyService(Company company);
+	
+	/**
+	 * Function declaration for getAllCompanyService that returns a list of all 
+	 * the companies in the table name Company of the database mescoe.
+	 * @return
+	 */
+	public List<Company>getAllCompanyService();
+	
+	/**
+	 * Function declaration for getCompanyService to get the details of a company based 
+	 * on the parameter passed which is company Id named cid in the table company of the database mescoe.
+	 * @param cid
+	 * @return
+	 */
+	public Company getCompanyService(int cid);
+	
+	/**
+	 * Function declaration for getCompanyByYear that returns a list of companies 
+	 * visited in the college in that particular year based on the year passed as the parameter.
+	 * @param year
+	 * @return
+	 */
+	public List<Company> getCompanyByYear(int year);
+	
+	/**
+	 * Function declaration for checkCompanyService to check whether the company exists in 
+	 * the table called Company of the database mescoe.
+	 * @param cid
+	 * @return
+	 */
+	public boolean checkCompanyService(int cid);
 }
